559d2d36114fd09150a411b3378043a9096c1779
Pichika Chandu <chandupichika0@gmail.com>
2025-12-29 22:43:23 +0530

    feat: login logout API

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signUp, signIn, getCurrentUser } = require("../controllers/authController");
+const { signUp, signIn, getCurrentUser, logout } = require("../controllers/authController");
 const { getMe } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.get("/me", getMe, getCurrentUser);
+router.post("/logout", logout);
 
 module.exports = router;
